62041232c492f270aa778c0f7bbe53ba7dc62427
santiagocala <s.cala@uniandes.edu.co>
2021-09-28 13:46:02 -0500

    Primera versión :(

---
+++ main.js
@@ -0,0 +1,7 @@
+let promise1 = fetch(https://gist.githubusercontent.com/josejbocanegra/9a28c356416badb8f9173daf36d1460b/raw/5ea84b9d43ff494fcbf5c5186544a18b42812f09/restaurant.json);
+
+
+const categoria = document.getElementById('categoria');
+
+//categoria.innerHTML = 'Nueva categoría'; 
+console.log('Se cambio la categoría');
